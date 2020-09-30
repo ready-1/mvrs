4f7512affbb32af90b306c662a669d401b4f1d24
Bob King <bob@getready1.com>
2020-09-30 12:02:35 -0400

    add external data file

---
+++ data.js
@@ -0,0 +1,13 @@
+data = {
+    "mv1_id": "843 6397 3835 ",
+    "mv1_passcode": "210188 ",
+    "mv1_url": "https: //us02web.zoom.us/j/84363973835?pwd=YXpRRjdwOEkrNkJRYmM4VkpNeDRxZz09",
+
+    "mv2_id": "826 5257 6038",
+    "mv2_passcode": "061249 ",
+    "var mv2_url": "https: //us02web.zoom.us/j/82652576038?pwd=Tk1COGp0cVJQYlplcUd3eDZrcm5sdz09",
+
+    "mv3_id": "939 9901 0887",
+    "mv3_passcode": "324275",
+    "mv3_url": "https: //zoom.us/j/93999010887?pwd=TmYyRVJ2bWFGdUJna3kxSEY3K3ZRQT09"
+}
