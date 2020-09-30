9c37cee77ac90125e77a824b5a5061d81580a2c6
Bob King <bob@getready1.com>
2020-09-30 15:52:38 -0400

    update mvr 1 9/30 1552

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 data = {
-    "mv1_id": "843 6397 3835 ",
-    "mv1_passcode": "210188 ",
-    "mv1_url": "https: //us02web.zoom.us/j/84363973835?pwd=YXpRRjdwOEkrNkJRYmM4VkpNeDRxZz09",
+    "mv1_id": "896 4511 4895",
+    "mv1_passcode": "724012",
+    "mv1_url": "https://us02web.zoom.us/j/89645114895?pwd=cVNpR0lZMjFEVDJmWmdxQmR5UzMydz09",
 
     "mv2_id": "826 5257 6038",
     "mv2_passcode": "061249 ",
-    "var mv2_url": "https: //us02web.zoom.us/j/82652576038?pwd=Tk1COGp0cVJQYlplcUd3eDZrcm5sdz09",
+    "var mv2_url": "https://us02web.zoom.us/j/82652576038?pwd=Tk1COGp0cVJQYlplcUd3eDZrcm5sdz09",
 
     "mv3_id": "939 9901 0887",
     "mv3_passcode": "324275",
-    "mv3_url": "https: //zoom.us/j/93999010887?pwd=TmYyRVJ2bWFGdUJna3kxSEY3K3ZRQT09"
+    "mv3_url": "https://zoom.us/j/93999010887?pwd=TmYyRVJ2bWFGdUJna3kxSEY3K3ZRQT09"
 }
